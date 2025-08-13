75842fce4b73567985877b5a493b61d1a5777720
shehryar-spotter <shehryar@spotter.ai>
2025-08-13 22:22:11 +0500

    Initialized Android and iOS Apps. Implemented Login and Signup UI.

--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -221,7 +221,7 @@ const styles = StyleSheet.create({
     },
     content: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'center'
     },
     title: {
         fontSize: 20,
